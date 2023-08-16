c46cb642343b9071ff968eeab2379d8fd50803ba
Sasha Isler <sasha.isler@ftsgps.com>
2023-08-15 20:12:29 -0400

    adding items works

--- exchange.js
+++
@@ -1,6 +0,0 @@
-const task = document.getElementById('task');
-const grade = document.getElementById('grade');
-const add = document.getElementById('add');
-
-const API_KEY="";
-const apiUrl="";
---
+++ popout.js
@@ -0,0 +1,92 @@
+document.querySelector('.create-todo').addEventListener('click', function(){
+    document.querySelector('.new-item').style.display='block';
+});
+
+document.querySelector('.new-item button').addEventListener('click', function(){
+    var itemName = document.querySelector('.new-item input').value;
+    if (itemName != '') {
+        var itemsStorage = localStorage.getItem('todo-items');
+        var itemsArr = [];
+        itemsArr = JSON.parse(itemsStorage);
+        itemsArr.push({"item":itemName, "status":0});
+        saveItems(itemsArr);
+        fetchItems();
+        document.querySelector('.new-item input').value='';
+        document.querySelector('.new-item').style.display='none';
+    }
+});
+
+function fetchItems() {
+    const itemsList = document.querySelector('ul.todo-items');
+    itemsList.innerHTML = '';
+    var newItemHTML = '';
+    try{
+        var itemsStorage = localStorage.getItem('todo-items');
+        var itemsArr = JSON.parse(itemsStorage);
+
+        for (var i = 0; i < itemsArr.length; i++) {
+            var status = '';
+            if (itemsArr[i].status == 1) {
+                status = 'class="done"';
+            }
+            newItemHTML += `<li data-itemindex="${i}" ${status}>
+            <div><span class="itemComplete">+</span><span class="item">${itemsArr[i].item}</span></div>
+            <span class="itemDelete">X</span></li>`;
+        }
+
+        itemsList.innerHTML = newItemHTML;
+
+        var itemsListUL = document.querySelectorAll('ul li');
+        for (var i = 0; i < itemsListUL.length; i++) {
+
+            itemsListUL[i].querySelector('.itemComplete').addEventListener('click', function() {
+                var index = this.parentNode.dataset.itemIndex;
+                itemComplete(index);
+            });
+            itemsListUL[i].querySelector('.itemDelete').addEventListener('click', function() {
+                var index = this.parentNode.parentNode.parentNode.dataset.itemIndex;
+                itemDelete(index);
+            });
+        }
+    } catch(e) {
+    }
+}
+
+function itemComplete(index) {
+    
+    var itemsStorage = localStorage.getItem('todo-items');
+
+    if (itemsStorage == null) {
+        itemsStorage = '[]';
+    }
+
+    var itemsArr = JSON.parse(itemsStorage);
+
+    itemsArr[index].status = 1;
+
+    saveItems(itemsArr);
+
+    document.querySelector('ul.todo-items li[data-itemindex="'+index+'"]').className='done';
+}
+function itemDelete(index) {
+    var itemsStorage = localStorage.getItem('todo-items');
+    if (itemsStorage == null) {
+        itemsStorage = '[]';
+    }
+    var itemsArr = JSON.parse(itemsStorage);
+
+    itemsArr.splice(index, 1);
+
+    saveItems(itemsArr);
+
+    document.querySelector('ul.todo-items li[data-itemindex="'+index+'"]').remove();
+
+}
+
+function saveItems(obj) {
+    var string = JSON.stringify(obj);
+    localStorage.setItem('todo-items', string);
+}
+
+fetchItems();
+
